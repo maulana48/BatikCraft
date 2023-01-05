bb5b27d3f3a0f93b6ea02a5edf44dd98fc088dff
maulana48 <hidarizero4869@gmail.com>
2023-01-06 04:03:15 +0700

    its end fortoday

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{ TestController, LandingController, AuthController };
 use App\Http\Livewire\{ Landing, Dashboard };
 
 /*
@@ -18,28 +17,6 @@ use App\Http\Livewire\{ Landing, Dashboard };
 Route::get('/w', function () {
     return view('welcome');
 });
+
 Route::get('/', Landing::class);
 Route::get('/dashboard', Dashboard::class);
-
-Route::prefix('/test')
-    ->name('auth.')
-    ->controller(AuthController::class)
-    ->group(function(){
-        Route::any('/login', 'login')->name('login');
-        Route::any('/logout', 'logout')->name('logout');
-        Route::any('/registration', 'registration')->name('registration');
-    });
-
-Route::prefix('/landing')
-    ->name('landing.')
-    ->controller(LandingController::class)
-    ->group(function(){
-        Route::get('/', 'index')->name('index');
-        Route::get('/show/{slug}', 'show')->name('show');
-        Route::get('/create', 'create')->name('create');
-        Route::get('/edit/{product}', 'edit')->name('edit');
-
-        Route::post('/store', 'store')->name('store');
-        Route::post('/update/{product}', 'update')->name('update');
-        Route::post('/destroy/{product}', 'destroy')->name('destroy');
-});
