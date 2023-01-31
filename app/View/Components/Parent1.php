9f3d2328a6c7b7a9248a9a2a2c300d02a1ddf071
maulana48 <hidarizero4869@gmail.com>
2023-01-31 08:34:25 +0700

    some change

--- app/View/Components/Parent1.php
+++ app/View/Components/Parent1.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class parent extends Component
+class Parent1 extends Component
 {
     /**
      * Create a new component instance.
@@ -23,6 +23,6 @@ class parent extends Component
      */
     public function render()
     {
-        return view('components.parent');
+        return view('components.parent1');
     }
 }
--- app/View/Components/Parent2.blade.php
+++ app/View/Components/Parent2.blade.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class parent.blade extends Component
+class Parent2 extends Component
 {
     /**
      * Create a new component instance.
@@ -23,6 +23,6 @@ class parent.blade extends Component
      */
     public function render()
     {
-        return view('components.parent.blade');
+        return view('components.parent2');
     }
 }
