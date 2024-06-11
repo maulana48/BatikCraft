71d928277b5f1ad22a7faf8c2a6693b54667b672
Maahir Maulana <mbismbisbbl@@gmail.com>
2024-06-12 06:26:09 +0700

    (+) cart : zero produk

--- resources/views/livewire/layouts/cart.blade.php
+++ resources/views/livewire/layouts/cart.blade.php
@@ -21,41 +21,46 @@
 
         <!-- wishlist -->
         <div class="col-span-9 space-y-4">
-            <div class="flex justify-end">
-                <button wire:click="checkOut" class="py-[10px] px-[20px] rounded-lg bg-blue-600 text-white">Check
-                    out</button>
-            </div>
-            <template x-for="(b, index) in batik_list">
-                <template x-if="b">
+            @if (count($batik_list) != 0)
+                <div class="flex justify-end">
+                    <button wire:click="checkOut" class="py-[10px] px-[20px] rounded-lg bg-blue-600 text-white">Check
+                        out</button>
+                </div>
+                @foreach ($batik_list as $key => $b)
                     <div class="flex items-center justify-between border gap-6 p-4 border-gray-200 rounded">
                         <div class="w-28">
-                            <img x-bind:src="b.main_media" :alt="'product ' + (index + 1)" class="w-full">
+                            <img src="{{ $b->main_media }}" alt="product {{ $key + 1 }}" class="w-full">
                         </div>
                         <div class="w-1/3">
-                            <h2 x-text="b.name" class="text-gray-800 text-xl font-medium uppercase"></h2>
-                            <p x-show="(b.stok != 0)" class="text-gray-500 text-sm">
-                                Stok : <span class="text-green-600">Tersedia</span>
-                            </p>
-                            <p x-show="(b.stok == 0)" class="text-gray-500 text-sm">
-                                Stok : <span class="text-red-600">Maaf produk ini sudah habis</span>
+                            <h2 class="text-gray-800 text-xl font-medium uppercase">{{ $b->name }}</h2>
+                            <p class="text-gray-500 text-sm">Stok :
+                                {!! $b->stock != 0
+                                    ? '<span class="text-green-600">Tersedia</span>'
+                                    : '<span class="text-red-600">Maaf produk ini sudah habis</span>' !!}
                             </p>
                             <p class="text-gray-500 text-sm">
-                                Jumlah : <span x-text="cartProducts[index].amount" class="text-gray-500"></span>
+                                Jumlah : <span class="text-gray-500">{{ $cartProducts[$key]->amount }}</span>
                             </p>
                         </div>
-                        <div x-text="'Rp.'  + parseInt(b.price)" class="text-[#6B4226] text-lg font-semibold"></div>
-                        <span x-text="console.log(b)"></span>
-                        <button x-on:click="b.status = await $wire.checking(index)"
-                            x-text="b.status == 1 ? 'Tekan untuk Check-out' : 'Checked-out'"
-                            class="px-6 py-2 text-center text-sm text-white bg-[#6B4226] border border-[#120a1b] rounded hover:bg-transparent hover:text-[#6B4226] transition uppercase font-roboto font-medium"></button>
+                        <div class="text-[#6B4226] text-lg font-semibold">{{ 'Rp.' . $b->price }} </div>
+                        <button wire:click="checking({{ $key }})"
+                            class="px-6 py-2 text-center text-sm text-white bg-[#6B4226] border border-[#120a1b] rounded hover:bg-transparent hover:text-[#6B4226] transition uppercase font-roboto font-medium">
+                            {{ $b->status == 1 ? 'Tekan untuk Check-out' : 'Checked-out' }}
+                        </button>
 
-                        <div x-on:click="deleted = confirm('Pindahkan produk dari keranjang?'); if(deleted) await $wire.delete(index); if(deleted) b = null;"
+                        <div @click="deleted = confirm('Pindahkan produk dari keranjang?'); if(deleted) await $wire.delete({{ $key }});"
                             class="text-gray-600 cursor-pointer hover:text-[#6B4226]">
                             <i class="fa-solid fa-trash"></i>
                         </div>
                     </div>
-                </template>
-            </template>
+                @endforeach
+            @else
+                <div class="flex items-center justify-between border gap-6 p-4 border-gray-200 rounded">
+                    <div class="w-full text-center">
+                        <p>Anda belum menambahkan produk</p>
+                    </div>
+                </div>
+            @endif
         </div>
         <!-- ./wishlist -->
 
--- resources/views/livewire/layouts/transaction.blade.php
+++ resources/views/livewire/layouts/transaction.blade.php
@@ -22,31 +22,29 @@
         <div class="col-span-9 space-y-4">
             @if (count($order_list) != 0)
                 @foreach ($order_list as $index => $item)
-                    @if ($item)
-                        <div class="flex items-center justify-between border gap-6 p-4 border-gray-200 rounded">
-                            <div class="w-28">
-                                <p>order {{ $index + 1 }}</p>
-                            </div>
-                            <div class="w-1/3">
-                                <h2 class="text-gray-800 text-xl font-medium uppercase">
-                                    {{ $item->order_timestamp->isoFormat('dddd, D MMMM Y') }}</h2>
-                                <p class="text-gray-500 text-sm">
-                                    Total Produk : <span class="text-gray-500">{{ count($item->orderProduct) }}</span>
-                                </p>
-                                <p class="text-gray-500 text-sm">Status :
-                                    {!! $item->status == 3
-                                        ? '<span class="text-green-600">Selesai</span>'
-                                        : '<span class="text-red-600">Menunggu pembayaran</span>' !!}
-                                </p>
-                            </div>
-                            <div class="text-[#6B4226] text-lg font-semibold">
-                                Rp.{{ $item->total_amount }}
-                            </div>
-                            <button wire:click="detailOrder({{ $item->id }})"
-                                class="px-6 py-2 text-center text-sm text-white bg-[#6B4226] border border-[#120a1b] rounded hover:bg-transparent hover:text-[#6B4226] transition uppercase font-roboto font-medium">Lihat
-                                detail</button>
+                    <div class="flex items-center justify-between border gap-6 p-4 border-gray-200 rounded">
+                        <div class="w-28">
+                            <p>order {{ $index + 1 }}</p>
                         </div>
-                    @endif
+                        <div class="w-1/3">
+                            <h2 class="text-gray-800 text-xl font-medium uppercase">
+                                {{ $item->order_timestamp->isoFormat('dddd, D MMMM Y') }}</h2>
+                            <p class="text-gray-500 text-sm">
+                                Total Produk : <span class="text-gray-500">{{ count($item->orderProduct) }}</span>
+                            </p>
+                            <p class="text-gray-500 text-sm">Status :
+                                {!! $item->status == 3
+                                    ? '<span class="text-green-600">Selesai</span>'
+                                    : '<span class="text-red-600">Menunggu pembayaran</span>' !!}
+                            </p>
+                        </div>
+                        <div class="text-[#6B4226] text-lg font-semibold">
+                            Rp.{{ $item->total_amount }}
+                        </div>
+                        <button wire:click="detailOrder({{ $item->id }})"
+                            class="px-6 py-2 text-center text-sm text-white bg-[#6B4226] border border-[#120a1b] rounded hover:bg-transparent hover:text-[#6B4226] transition uppercase font-roboto font-medium">Lihat
+                            detail</button>
+                    </div>
                 @endforeach
             @else
                 <div class="flex items-center justify-between border gap-6 p-4 border-gray-200 rounded">
